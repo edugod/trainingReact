e42ccaf83168028d86a7349721aa0696aaa257e1
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-12-25 19:12:19 -0300

    muito bom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Header from './components/Header/Header'
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <Header/>
+    </div>
   )
 }
 

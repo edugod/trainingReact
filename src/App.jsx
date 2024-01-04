001fd1fa14201b8f8692f2fc15a3d81706a2d092
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2024-01-03 21:21:41 -0300

    estoy cansado jefe

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import './App.css' 
+import Companies from './components/Companies/Companies'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Header/>
       <Hero />
       </div>
+      <Companies/>
     </div>
   )
 }

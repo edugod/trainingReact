45bffad024ba6732de0a1bd53e2a26725bfb5625
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2024-01-03 21:30:34 -0300

    deu por hoje.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
-import './App.css' 
+import './App.css'
 import Companies from './components/Companies/Companies'
-
+import Residencies from './components/Residencies/Residencies'
 
 const App = () => {
-  return (
-    <div className='App'>
-      <div>
-        <div className="white-gradient"/>
-      <Header/>
-      <Hero />
-      </div>
-      <Companies/>
-    </div>
-  )
+	return (
+		<div className='App'>
+			<div>
+				<div className='white-gradient' />
+				<Header />
+				<Hero />
+			</div>
+			<Companies />
+			<Residencies />
+		</div>
+	)
 }
 
 export default App
--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import './Residencies.css'
-import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
 import 'swiper/css'
 import data from '../../utils/slider.json'
 
 const Residencies = () => {
-  return (
-        <section className="r-wrapper">
-            residencies
-        </section>
-    )
+	return (
+		<section className='r-wrapper'>
+			<div className='paddings innerWidth r-container'>
+				<div className='r-head flexColStart'>
+					<span className='orangeText'>Best Choices</span>
+					<span className='primaryText'>Popular Residencies</span>
+				</div>
+
+				<Swiper>
+					{data.map((card, i) => (
+						<SwiperSlide key={i}>
+							<div className='r-card'>
+								<img src={card.image} alt='home' />
+
+								<span className='secondaryText r-price'>
+									<span style={{ color: 'orange' }}>$</span>
+									<span>{card.price}</span>
+								</span>
+
+								<span className='primaryText'>{card.name}</span>
+								<span className='secondaryText'>{card.detail}</span>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+		</section>
+	)
 }
 
 export default Residencies
